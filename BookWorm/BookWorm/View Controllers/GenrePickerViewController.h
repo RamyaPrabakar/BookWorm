b95affaf2ace13e67d51455ba7d8bcacfd47266f
RamyaPrabakar <ramyaprabakar01@gmail.com>
2022-07-06 13:57:14 -0700

    Implemented basic login and sign up feature

---
+++ BookWorm/BookWorm/View Controllers/GenrePickerViewController.h
@@ -0,0 +1,16 @@
+//
+//  GenrePickerViewController.h
+//  BookWorm
+//
+//  Created by Ramya Prabakar on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GenrePickerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
